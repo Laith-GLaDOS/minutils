96dc300741c417c87fb4e5ebcdd7dc0ceda906db
Laith Rafid <usr.bin.ducc.2@gmail.com>
2022-02-19 23:11:36 +0300

    again, fixed broken code

--- mkdir/src/main.rs
+++ mkdir/src/main.rs
@@ -16,20 +16,42 @@ fn main() {
             }
         }
     }
-    for arg in env::args().skip(2) {
-        match fs::create_dir(&arg) {
-            Ok(_) => {
-                if verbose {
-                    println!("mkdir: created directory '{}'", &arg);
+    if verbose { // if there was a -v parameter, skip both mkdir bin name and -v
+        for arg in env::args().skip(2) {
+            match fs::create_dir(&arg) {
+                Ok(_) => {
+                    if verbose {
+                        println!("mkdir: created directory '{}'", &arg);
+                    }
+                },
+                Err(e) => {
+                    if e.kind() == ErrorKind::AlreadyExists {
+                        println!("mkdir: cannot create directory '{}': File exists", &arg);
+                    } else if e.kind() == ErrorKind::PermissionDenied {
+                        eprintln!("mkdir: cannot create directory '{}': Permission denied", arg);
+                    } else {
+                        eprintln!("mkdir: cannot create directory '{}': No space left on device", arg);
+                    }
                 }
-            },
-            Err(e) => {
-                if e.kind() == ErrorKind::AlreadyExists {
-                    println!("mkdir: cannot create directory '{}': File exists", &arg);
-                } else if e.kind() == ErrorKind::PermissionDenied {
-                    eprintln!("mkdir: cannot create directory '{}': Permission denied", arg);
-                } else {
-                    eprintln!("mkdir: cannot create directory '{}': No space left on device", arg);
+            }
+        }
+    }
+    else { // only skip mkdir bin name
+        for arg in env::args().skip(1) {
+            match fs::create_dir(&arg) {
+                Ok(_) => {
+                    if verbose {
+                        println!("mkdir: created directory '{}'", &arg);
+                    }
+                },
+                Err(e) => {
+                    if e.kind() == ErrorKind::AlreadyExists {
+                        println!("mkdir: cannot create directory '{}': File exists", &arg);
+                    } else if e.kind() == ErrorKind::PermissionDenied {
+                        eprintln!("mkdir: cannot create directory '{}': Permission denied", arg);
+                    } else {
+                        eprintln!("mkdir: cannot create directory '{}': No space left on device", arg);
+                    }
                 }
             }
         }
