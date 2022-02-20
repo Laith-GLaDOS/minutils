59cf65221ab76533b5ec07da15238d60fd497f2f
Laith Rafid <usr.bin.ducc.2@gmail.com>
2022-02-20 18:29:27 +0300

    fixed bug

--- yes/src/main.rs
+++ yes/src/main.rs
@@ -1,10 +1,22 @@
 use std::env;
 
 fn main() {
-    loop {
-        for arg in env::args().skip(1) {
-            print!("{} ", arg);
+    let mut two_args = false;
+    let args: Vec<String> = env::args().collect();
+    if args.len() >= 2 {
+        two_args = true;
+    }
+    if two_args {
+        loop {
+            for arg in env::args().skip(1) {
+                print!("{} ", arg);
+            }
+            println!();
         }
-        println!();
+    }
+    else {
+        loop {
+            println!("y");
+        }
     }
 }
