ce2e68fbb98fdd4bbc1af78ea518cf39b909a201
Laith Rafid <usr.bin.ducc.2@gmail.com>
2022-02-20 18:07:10 +0300

    fixed bug

--- cat/src/main.rs
+++ cat/src/main.rs
@@ -5,7 +5,7 @@ fn main() {
     for arg in env::args().skip(1) {
         let contents = fs::read_to_string(&arg); // read file contents
         match contents {
-            Ok(contents) => println!("{}", contents), // print contents if read was successful
+            Ok(contents) => print!("{}", contents), // print contents if read was successful
             Err(e) => { // if read was unsuccessful
                 if e.kind() == std::io::ErrorKind::NotFound { // if file does not exist
                     println!("cat: {}: No such file or directory", &arg); // print no file message
